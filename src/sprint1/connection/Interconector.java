25e29631e4c310f20e5d6943e3aa113c3f439da3
javie <javie@DESKTOP-GNVG7TV>
2020-10-03 16:59:10 +0200

    Probando las ramas

--- src/sprint1/connection/Interconector.java
+++
@@ -1,5 +0,0 @@
-package sprint1.connection;
-
-public class Interconector {
-	public static String URL = "jdbc:sqlite:C:\\Users\\javie\\Desktop\\master\\sprint1\\resources\\bdProject.db";
-}
--- src/sprint1/ui/AdminWindow.java
+++ src/sprint1/ui/AdminWindow.java
@@ -1,7 +1,6 @@
 package sprint1.ui;
 
 import javax.swing.JDialog;
-import javax.swing.JOptionPane;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -14,7 +13,6 @@ public class AdminWindow extends JDialog {
 	private static final long serialVersionUID = 1L;
 	private MainWindow parent = null;
 	private JButton btnReturn;
-	private JButton btnAsignarMonitor;
 
 	/**
 	 * Create the dialog.
@@ -24,7 +22,6 @@ public class AdminWindow extends JDialog {
 		this.parent = mainWindow;
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
-		getContentPane().add(getBtnAsignarMonitor());
 		getContentPane().add(getBtnReturn());
 	}
 
@@ -38,13 +35,5 @@ public class AdminWindow extends JDialog {
 			});
 		}
 		return btnReturn;
-	}
-
-	//wip
-	private JButton getBtnAsignarMonitor() {
-		if (btnAsignarMonitor == null) {
-			btnAsignarMonitor = new JButton("Asignar monitor a actividad");
-		}
-		return btnAsignarMonitor;
 	}
 }
