5a5ad718a68ed3fa130af156f568d07e7b7be5cc
uo271773 <uo271773@uniovi.es>
2020-12-01 17:37:26 +0100

    Finalizada historia

--- src/sprint1/ui/ventanas/administracion/actividades/CancelarActividadDialog.java
+++ src/sprint1/ui/ventanas/administracion/actividades/CancelarActividadDialog.java
@@ -2,25 +2,31 @@ package sprint1.ui.ventanas.administracion.actividades;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import sprint1.business.dominio.centroDeportes.actividades.Actividad;
 import sprint1.business.dominio.centroDeportes.actividades.ActividadPlanificada;
-import sprint1.business.dominio.centroDeportes.reservas.Reserva;
 import sprint1.ui.ventanas.administracion.util.CalendarioSemanalCancelar;
 
 public class CancelarActividadDialog extends JDialog {
@@ -42,23 +48,29 @@ public class CancelarActividadDialog extends JDialog {
 	private JPanel pnButtons;
 	private JRadioButton rdbtnDiaConcreto;
 	private JLabel lblOpcion;
+	private JRadioButton rdbtnTodasActividades;
+	private Actividad actividadBase;
+	private JScrollPane scrollPane;
+	private DefaultListModel<ActividadPlanificada> listaModelo;
 
 	/**
 	 * Create the dialog.
 	 */
 	public CancelarActividadDialog(CalendarioSemanalCancelar parent, ActividadPlanificada actividadPlanificada) {
+		setResizable(false);
 		this.parent = parent;
 		this.actividad = actividadPlanificada;
+		this.actividadBase = getActividadBase();
+		this.scrollPane = new JScrollPane();
+		this.listaModelo = new DefaultListModel<ActividadPlanificada>();
 		setTitle("Cancelar actividad");
 		getContentPane().setBackground(Color.WHITE);
-		setBounds(100, 100, 440, 300);
+		setBounds(100, 100, 429, 461);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(Color.WHITE);
-		FlowLayout fl_contentPanel = new FlowLayout();
-		fl_contentPanel.setAlignment(FlowLayout.LEFT);
-		contentPanel.setLayout(fl_contentPanel);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		contentPanel.add(getPnActividad());
 		contentPanel.add(getPnButtons());
 		{
@@ -72,7 +84,7 @@ public class CancelarActividadDialog extends JDialog {
 				okButton.setBackground(new Color(34, 139, 34));
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
-						cancelarActividad();
+						cancelarActividades();
 					}
 				});
 				okButton.setActionCommand("OK");
@@ -95,6 +107,15 @@ public class CancelarActividadDialog extends JDialog {
 		
 		ButtonGroup group = new ButtonGroup();
 		group.add(rdbtnDiaConcreto);
+		group.add(rdbtnTodasActividades);
+	}
+	protected void cancelarActividades() {
+		if (rdbtnDiaConcreto.isSelected()) {
+			cancelarActividad();
+		}
+		else if (rdbtnTodasActividades.isSelected()) {
+			cancelarGrupoActividades();
+		}
 	}
 	protected void cancelarActividad() {
 		if (JOptionPane.showConfirmDialog(this, "¿Seguro que quiere eliminar esta actividad y las reservas relacionadas?") == JOptionPane.YES_OPTION) {
@@ -107,6 +128,21 @@ public class CancelarActividadDialog extends JDialog {
 			JOptionPane.showMessageDialog(this, "Actividad y reservas eliminadas correctamente.");
 			dispose();
 		}
+	}
+	private void cancelarGrupoActividades() {
+		if (JOptionPane.showConfirmDialog(this, "¿Seguro que quiere eliminar esta actividad y las actividades y reservas relacionadas?") == JOptionPane.YES_OPTION) {
+			for(int i = 0; i < listaModelo.getSize(); i++) {
+				try {
+					parent.getPrograma().eliminarActividadPlanificada(listaModelo.get(i));
+					parent.getPrograma().eliminarReserva(listaModelo.get(i).getCodigoActividad());
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(this, "Ha habido un error eliminando la actividad planificada. Por favor, póngase en contacto con el administrador");
+				}
+			}
+			JOptionPane.showMessageDialog(this, "Actividades y reservas eliminadas correctamente.");
+			dispose();
+		}
+		
 	}
 	private JPanel getPnActividad() {
 		if (pnActividad == null) {
@@ -145,14 +181,14 @@ public class CancelarActividadDialog extends JDialog {
 	}
 	private JLabel getLblNombreActividad() {
 		if (lblNombreActividad == null) {
-			lblNombreActividad = new JLabel(nombreActividad());
+			lblNombreActividad = new JLabel(actividadBase.getNombre());
 		}
 		return lblNombreActividad;
 	}
-	private String nombreActividad() {
+	private Actividad getActividadBase() {
 		for (Actividad a : parent.getPrograma().getActividades()) {
 			if (a.getCodigo().equals(actividad.getCodigoActividad())) {
-				return a.getNombre();
+				return a;
 			}
 		}
 		return null;
@@ -173,15 +209,42 @@ public class CancelarActividadDialog extends JDialog {
 		if (pnButtons == null) {
 			pnButtons = new JPanel();
 			pnButtons.setBackground(Color.WHITE);
-			pnButtons.setLayout(new GridLayout(0, 1, 0, 0));
-			pnButtons.add(getLblOpcion());
-			pnButtons.add(getRdbtnDiaConcreto());
+			GridBagLayout gbl_pnButtons = new GridBagLayout();
+			gbl_pnButtons.columnWidths = new int[] {160, 200, 0};
+			gbl_pnButtons.rowHeights = new int[]{25, 25, 0};
+			gbl_pnButtons.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+			gbl_pnButtons.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+			pnButtons.setLayout(gbl_pnButtons);
+			GridBagConstraints gbc_lblOpcion = new GridBagConstraints();
+			gbc_lblOpcion.fill = GridBagConstraints.BOTH;
+			gbc_lblOpcion.insets = new Insets(0, 0, 5, 5);
+			gbc_lblOpcion.gridx = 0;
+			gbc_lblOpcion.gridy = 0;
+			pnButtons.add(getLblOpcion(), gbc_lblOpcion);
+			GridBagConstraints gbc_rdbtnDiaConcreto = new GridBagConstraints();
+			gbc_rdbtnDiaConcreto.fill = GridBagConstraints.BOTH;
+			gbc_rdbtnDiaConcreto.insets = new Insets(0, 0, 0, 5);
+			gbc_rdbtnDiaConcreto.gridx = 0;
+			gbc_rdbtnDiaConcreto.gridy = 1;
+			pnButtons.add(getRdbtnDiaConcreto(), gbc_rdbtnDiaConcreto);
+			GridBagConstraints gbc_rdbtnTodasActividades = new GridBagConstraints();
+			gbc_rdbtnTodasActividades.fill = GridBagConstraints.BOTH;
+			gbc_rdbtnTodasActividades.gridx = 1;
+			gbc_rdbtnTodasActividades.gridy = 1;
+			pnButtons.add(getRdbtnTodasActividades(), gbc_rdbtnTodasActividades);
 		}
 		return pnButtons;
 	}
 	private JRadioButton getRdbtnDiaConcreto() {
 		if (rdbtnDiaConcreto == null) {
 			rdbtnDiaConcreto = new JRadioButton("Solo el d\u00EDa seleccionado");
+			rdbtnDiaConcreto.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					contentPanel.remove(scrollPane);
+					contentPanel.validate();
+					contentPanel.repaint();
+				}
+			});
 			rdbtnDiaConcreto.setSelected(true);
 			rdbtnDiaConcreto.setBackground(Color.WHITE);
 		}
@@ -193,5 +256,31 @@ public class CancelarActividadDialog extends JDialog {
 			lblOpcion.setFont(new Font("Tahoma", Font.BOLD, 16));
 		}
 		return lblOpcion;
+	}
+	private JRadioButton getRdbtnTodasActividades() {
+		if (rdbtnTodasActividades == null) {
+			rdbtnTodasActividades = new JRadioButton("Todas las actividades de este tipo");
+			rdbtnTodasActividades.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					muestraActividades();
+				}
+			});
+			rdbtnTodasActividades.setBackground(Color.WHITE);
+		}
+		return rdbtnTodasActividades;
+	}
+	protected void muestraActividades() {
+		scrollPane.setBackground(Color.white);
+		for (ActividadPlanificada ap : parent.getPrograma().getActividadesPlanificadas()) {
+			if (ap.getCodigoActividad().equals(actividadBase.getCodigo())) {
+				listaModelo.addElement(ap);
+			}
+		}
+		JList<ActividadPlanificada> lista = new JList<ActividadPlanificada>(listaModelo);
+		scrollPane.setViewportView(lista);
+		scrollPane.setPreferredSize(new Dimension(395,250));
+		contentPanel.add(scrollPane);
+		contentPanel.validate();
+		contentPanel.repaint();
 	}
 }
